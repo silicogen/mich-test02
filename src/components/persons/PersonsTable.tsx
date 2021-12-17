8724747076f16b3bc94e6fb4e7615bcbd19549d0
Alexey <alexey.chuprov@gmail.com>
2021-12-17 17:01:21 +0300

    load 32, 1000

--- src/components/persons/PersonsTable.tsx
+++ src/components/persons/PersonsTable.tsx
@@ -1,35 +1,43 @@
 
 import { useEffect } from "react";
-// import { data3 } from "../../data/data3";
-// import { data32 } from "../../data/data32";
 import { useRoot } from "../../store/Root";
 import { useLoad } from "./handlers"
 
 export const PersonsTable = () => {
-    const root = useRoot();;
+    const root = useRoot();
     const persons = root.persons;
     const [axiRes, load] = useLoad();
-    useEffect(load, []);
+    useEffect(() => load(10), []);
     persons.setItems(axiRes.data);
-    return <table>
-        <thead>
-            <tr>
-                <th> id</th>
-                <th>firstName</th>
-                <th>lastName</th>
-                <th>email</th>
-                <th>phone</th>
-            </tr>
-        </thead>
-        <tbody>
-            {persons.items.map(r =>
-                <tr id={r.id.toString()}>
-                    <td> {r.id}</td>
-                    <td> {r.firstName}</td>
-                    <td> {r.lastName}</td>
-                    <td> {r.email}</td>
-                    <td> {r.phone}</td>
-                </tr>)}
-        </tbody>
-    </table>
+    return <>
+        <button
+            onClick={() => load(32)}
+        >Загрузить 32
+        </button>
+        <button
+            onClick={() => load(1000)}
+        >Загрузить 1000
+        </button>
+        <table>
+            <thead>
+                <tr>
+                    <th> id</th>
+                    <th>firstName</th>
+                    <th>lastName</th>
+                    <th>email</th>
+                    <th>phone</th>
+                </tr>
+            </thead>
+            <tbody>
+                {persons.items.map(r =>
+                    <tr id={r.id.toString()}>
+                        <td> {r.id}</td>
+                        <td> {r.firstName}</td>
+                        <td> {r.lastName}</td>
+                        <td> {r.email}</td>
+                        <td> {r.phone}</td>
+                    </tr>)}
+            </tbody>
+        </table>
+    </>
 }
