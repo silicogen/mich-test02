aa0b6fe53331df68693354211eab0db9995325da
Alexey <alexey.chuprov@gmail.com>
2021-12-17 14:58:45 +0300

    fill data static

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { PersonsTable } from "./components/persons/PersonsTable"
 function App() {
   return (
     <div className="App">
-      Начальная версия
       <PersonsTable />
     </div>
   );
--- src/components/persons/PersonsTable.tsx
+++ src/components/persons/PersonsTable.tsx
@@ -1,4 +1,32 @@
 
-export const PersonsTable = ()=><div>
-    Персонс
-</div>
+import { data3 } from "../../data/data3";
+// import { data32 } from "../../data/data32";
+// import { data1000 } from "../../data/data1000";
+const data = data3;
+export const PersonsTable = () => <table>
+    <thead>
+        <tr>
+            <th> id</th>
+            <th>firstName</th>
+            <th>lastName</th>
+            <th>email</th>
+            <th>phone</th>
+        </tr>
+    </thead>
+    <tbody>
+        {data.map(r => <tr>
+            <td> {r.id}</td>
+            <td> {r.firstName}</td>
+            <td> {r.lastName}</td>
+            <td> {r.email}</td>
+            <td> {r.phone}</td>
+            {/* <td> {r.address.streetAddress}</td>
+            <td> {r.address.city}</td>
+            <td> {r.address.state}</td>
+            <td> {r.address.zip}</td>
+            <td> {r.description}</td> */}
+        </tr>)}
+    </tbody>
+
+
+</table>
